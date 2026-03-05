1f32104e81e29c2e4d7d1f32c7da7016e8797aa7
vibhor kumar <vibhork1105@gmail.com>
2026-03-06 01:02:14 +0530

    task0, task1, task2, task3

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -3,7 +3,6 @@
 import math
 
 # ## Task 0.1
-from typing import Callable, Iterable
 
 #
 # Implementation of a prelude of elementary functions.
@@ -33,9 +32,90 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.1.
+def mul(x: float, y: float) -> float:
+    """Multiptly two numbers"""
+    return x * y
 
 
-# ## Task 0.3
+def id(x: float) -> float:
+    """Return input"""
+    return x
+
+
+def add(x: float, y: float) -> float:
+    """Add two numbers"""
+    return x + y
+
+
+def neg(x: float) -> float:
+    """Return Negative of input"""
+    return -x
+
+
+def lt(x: float, y: float) -> float:
+    """Less than"""
+    return float(x < y)
+
+
+def eq(x: float, y: float) -> float:
+    """Equality comparison"""
+    return float(x == y)
+
+
+def max(x: float, y: float) -> float:
+    """Max of two numbers"""
+    return x if x > y else y
+
+
+def is_close(x: float, y: float) -> float:
+    """Two numbers close to each other"""
+    return float(abs(x - y) < 1e-2)
+
+
+def sigmoid(x: float) -> float:
+    """Sigmoid activation function"""
+    if x >= 0:
+        return 1.0 / (1.0 + math.exp(-x))
+    else:
+        return math.exp(x) / (1.0 + math.exp(x))
+
+
+def relu(x: float) -> float:
+    """ReLU activation function"""
+    return x if x > 0 else 0.0
+
+
+def log(x: float) -> float:
+    """Natural logarithm of number"""
+    return math.log(x)
+
+
+def exp(x: float) -> float:
+    """Exponential of number"""
+    return math.exp(x)
+
+
+def inv(x: float) -> float:
+    """Reciprocal of x"""
+    return 1 / x
+
+
+def log_back(x: float, d: float) -> float:
+    r"""$log(x)*d$"""
+    return (1 / x) * d
+
+
+def inv_back(x: float, d: float) -> float:
+    r"""$d/x$"""
+    return -1 / (x * x) * d
+
+
+def relu_back(x: float, d: float) -> float:
+    """D if x>0 else 0"""
+    return d if x > 0 else 0.0
+
+
+## Task 0.3
 
 # Small practice library of elementary higher-order functions.
 
@@ -50,5 +130,20 @@ from typing import Callable, Iterable
 # - sum: sum lists
 # - prod: take the product of lists
 
-
 # TODO: Implement for Task 0.3.
+def negList(ls: list[float]) -> list[float]:
+    """Negate a list of numbers"""
+    return [neg(x) for x in ls]
+
+def addLists(ls1: list[float], ls2: list[float]) -> list[float]:
+    """Add two lists of numbers together"""
+    return [add(x, y) for x, y in zip(ls1, ls2)]
+
+def sum(ls: list[float]) -> float:
+    """Sum a list of numbers"""
+    return reduce(lambda x,y:x*y, ls) 
+
+def prod(ls:list[float])-> float:
+    """Products of all numbers in a list"""
+    return reduce(lambda x,y:x*y,ls)
+
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -107,42 +107,37 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
-
+    s=sigmoid(a)
+    assert 0.0<=s<=1.0
+    assert_close(1-s,sigmoid(-a))
+    assert sigmoid(0)==0.5
+    assert sigmoid(a+1.0)>s
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
-
+    if a<b and b<c:
+        assert a<c
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+def test_symmetric(a:float,b:float) -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
-
+    assert_close(mul(a,b),mul(b,a))
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+def test_distribute(a:float,b:float,c:float) -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
-
+    assert_close(mul(add(a,b),c),add(mul(a,c),mul(b,c)))
 
 @pytest.mark.task0_2
-def test_other() -> None:
+def test_other(a:float) -> None:
     """Write a test that ensures some other property holds for your functions."""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
-
+    assert_close(neg(neg(a)),a) 
 
 # ## Task 0.3  - Higher-order functions
 
